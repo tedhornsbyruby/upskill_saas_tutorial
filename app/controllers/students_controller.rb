8490b724dfd76074791009a9148baf52442f8a3a
Ted Hornsby <palomino3922@gmail.com>
2017-03-07 14:36:20 +0000

    Added create action to save to the database

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,19 @@
 class StudentsController < ApplicationController
   def new
-      
+      @student = Student.new
   end
-
+  
+  def create
+     @student =  Student.new(student_params)
+     if @student.save
+         redirect_to new_student_path, notice: "Student sent"
+     else
+         redirect_to new_student_path, notice: "ERROR"
+     end
+  end
+  
+  private
+  def student_params
+      params.require(:student).permit(:name, :email, :comments)
+  end
 end
