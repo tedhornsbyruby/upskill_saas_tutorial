3058441a21a24fc326ce840e5a7ccf103775dbc6
Ted Hornsby <palomino3922@gmail.com>
2017-05-16 13:40:03 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   
   # GET to /users/:user_id./profile/new
   def new
-    # Render a blank profile details form
+    @profile = Profile.new
   end
   
 end
